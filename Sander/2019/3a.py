f601e72d062bcbcb1317ab6741d24f5d29b7c069
Sander van Rijn <svr003@gmail.com>
2019-12-04 10:46:25 +0100

    Added day 3

---
+++ Sander/2019/3a.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+directions = {'R': (0, 1), 'L': (0, -1), 'U': (-1, 0), 'D': (1, 0)}
+
+
+def make_step_tuples(start, step):
+    """returns tuples of all visited (np.array) coordinates through this step
+    
+    `start` is expected to be an (x, y) tuple
+    `step` is expected as a string: 'Dnnnn' where 'O' is an orientation (RLUD),
+    and nnnn is a nonzero integer
+
+    Example
+    >>> make_step_tuples((0,0), (L3))
+    [(0,-1), (0, -2), (0, -3)]"""
+    x, y = start
+    dx, dy = directions[step[0]]
+    steps = []
+    for _ in range(int(step[1:])):
+        x, y = x+dx, y+dy
+        steps.append((x, y))
+    return steps
+
+
+def make_step_set(instructions):
+    start = (0,0)
+    steps = []
+    for step in instructions:
+        steps.extend(make_step_tuples(start, step))
+        start = steps[-1]
+    return set(steps)
+
+
+    
+with open('input3.txt') as f:
+    w1 = next(f).strip().split(',')
+    w2 = next(f).strip().split(',')
+
+s1 = make_step_set(w1)
+s2 = make_step_set(w2)
+crossings = s1.intersection(s2)
+dists = [abs(x)+abs(y) for x, y in crossings]
+print(min(dists))

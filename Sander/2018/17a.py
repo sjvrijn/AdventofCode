7471291b8ca833315b23dc7582d4fe5c498161dc
Sander van Rijn <svr003@gmail.com>
2020-01-09 22:42:12 +0100

    Improvement of 2018/day 17a: almost-correct recursive implementation

--- Sander/2018/17a.py
+++ Sander/2018/17a.py
@@ -4,13 +4,26 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 xmax, ymax = 1001, 1850
+### CONSTANTS ###
+# blocks
 WATER = 0
 CLAY = 1
+# directions
+LEFT = -1
+RIGHT = 1
+# return codes
+OFF_MAP = 0
+HITWALL = 1
 
+# plotting helpers
+plot_start = 2320
+plot_interval = 5
 disp_dpi = 50
-disp_range = (slice(0, 30, 1), slice(450, 550, 1))
+disp_range = (slice(0, 1820, 1), slice(250, 700, 1))
 save_format = 'png'
 
+x_disp, y_disp = (disp_range[1].stop - disp_range[1].start,
+                  disp_range[0].stop - disp_range[0].start)
 
 coords = []
 with open('input17.txt') as f:
@@ -27,52 +40,111 @@ with open('input17.txt') as f:
             coords.append((vals, val))
 
 ground = np.full((ymax, xmax), fill_value=np.nan)
-sources = [(0, 500)]
+source = (0, 500)
 
 for c in coords:
     ground[c] = CLAY
-ground[sources[0]] = WATER
+ymax = np.max((ground == CLAY).nonzero()[0])
+
+ground[source] = WATER
 
 plt.figure(figsize=((xmax/disp_dpi)+1, (ymax/disp_dpi)+1))
 plt.imshow(ground[disp_range], cmap='viridis')
+plt.tight_layout()
 plt.savefig('17a_init.{}'.format(save_format))
 
-counter = 0
-while sources and counter < 10:
-    counter += 1
-    y, x = sources.pop(0)
-    print("checking source {}, {}...".format(y, x))
-    print("dropping down")
-    for y_idx in range(y+1, ymax):
-        if ground[y_idx, x] == CLAY:
-            break
+count = 0
+
+def recursive_dinges(source, direction):
+    y, x = source
+
+    global count
+    global ground
+
+    print(count)
+    if count >= plot_start and count % plot_interval == 0:
+        plt.imshow(ground[disp_range], cmap='viridis_r')
+        plt.tight_layout()
+        plt.savefig('17a-step-{}.{}'.format(count, save_format))
+        #input("waiting...")
+
+    count += 1
+
+    # Flow sideways
+    if direction != 0:
+        while x in range(0, xmax):
+            if ground[y, x] == CLAY:
+                dir_txt = 'left' if direction == LEFT else 'right'
+                #print("hit wall: {}".format(dir_txt))
+                return HITWALL
+            ground[y, x] = WATER
+
+            if np.isnan(ground[y+1, x]):
+                break
+            x += direction
+
+    # Flow down
+    while np.isnan(ground[y+1, x]):
+        y += 1
+        ground[y, x] = WATER
+        if y > ymax:
+            print("off map (going down)")
+            return OFF_MAP
+
+    # Flow splits & moves up while contained
+    done = False
+    while not done:
+        val1 = recursive_dinges((y, x+LEFT), LEFT)
+        val2 = recursive_dinges((y, x+RIGHT), RIGHT)
+
+        if val1 == HITWALL and val2 == HITWALL:
+            print("moving up")
+            y -= 1
         else:
-            ground[y_idx, x] = WATER
-    y = y_idx-1
+            done = True
 
-    print("going right")
-    for x_idx in range(x+1, xmax):
-        if ground[y, x_idx] == CLAY:
-            break
-        else:
-            ground[y, x_idx] = WATER
+    print("off map (recursive)")
+    return OFF_MAP
 
-        if ground[y+1, x_idx] != CLAY:
-            sources.append((y, x_idx))
-            break
 
-    print("going left")
-    for x_idx in range(x-1, 0, -1):
-        if ground[y, x_idx] == CLAY:
-            break
-        else:
-            ground[y, x_idx] = WATER
+recursive_dinges(source, 0)
+print(np.sum(ground == WATER))
 
-        if ground[y+1, x_idx] != CLAY:
-            sources.append((y, x_idx))
-            break
 
-    #plt.imshow(ground[disp_range], cmap='viridis_r')
-    #plt.savefig('17a-{}-hor.{}'.format(counter, save_format))
-    #input("waiting...")
+
+#counter = 0
+#while sources and counter < 10:
+#    counter += 1
+#    y, x = sources.pop(0)
+#    print("checking source {}, {}...".format(y, x))
+#    print("dropping down")
+#    for y_idx in range(y+1, ymax):
+#        if ground[y_idx, x] == CLAY:
+#            break
+#        else:
+#            ground[y_idx, x] = WATER
+#    y = y_idx-1
+#
+#    print("going right")
+#    for x_idx in range(x+1, xmax):
+#        if ground[y, x_idx] == CLAY:
+#            break
+#        else:
+#            ground[y, x_idx] = WATER
+#
+#        if ground[y+1, x_idx] != CLAY:
+#            sources.append((y, x_idx))
+#            break
+#
+#    print("going left")
+#    for x_idx in range(x-1, 0, -1):
+#        if ground[y, x_idx] == CLAY:
+#            break
+#        else:
+#            ground[y, x_idx] = WATER
+#
+#        if ground[y+1, x_idx] != CLAY:
+#            sources.append((y, x_idx))
+#            break
+
 

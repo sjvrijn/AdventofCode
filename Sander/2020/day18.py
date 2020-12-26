807ec33f719d3bcd2b04ca27939b0a4480922156
Sander van Rijn <svr003@gmail.com>
2020-12-27 00:32:40 +0100

    Added 2020, day 18

---
+++ Sander/2020/day18.py
@@ -0,0 +1,68 @@
+from itertools import product
+import re
+
+from parse import parse
+import numpy as np
+
+
+def eval_new_math(line):
+    val = line.pop(0)
+    if val == '(':
+        val = eval_new_math(line)
+
+    while line:
+        operator = line.pop(0)
+        if operator == ')':
+            return val
+        val2 = line.pop(0)
+        if val2 == '(':
+            val2 = eval_new_math(line)
+        val = int(val)*int(val2) if operator == '*' else int(val)+int(val2)
+    return val
+
+
+def eval_newer_math(line):
+    val = line.pop(0)
+    if val == '(':
+        val = eval_newer_math(line)
+
+    val = int(val)
+    prev_val = None
+    while line:
+        operator = line.pop(0)
+        if operator == ')':
+            if prev_val:
+                val *= prev_val
+            return val
+        next_val = line.pop(0)
+        if next_val == '(':
+            next_val = eval_newer_math(line)
+
+        if operator == '+':
+            val += int(next_val)
+        elif operator == '*':
+            if prev_val:
+                prev_val *= val
+            else:
+                prev_val = val
+            val = int(next_val)
+
+    if prev_val:
+        val *= prev_val
+    return val
+
+
+def a(lines):
+    return sum(eval_new_math(list(line)) for line in lines)
+
+
+def b(lines):
+    return sum(eval_newer_math(list(line)) for line in lines)
+
+
+if __name__ == '__main__':
+    with open('input18.txt') as f:
+        lines = [line.strip().replace(' ', '') for line in f]
+
+    print(a(lines))
+    print(b(lines))

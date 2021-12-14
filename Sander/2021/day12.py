01ac38145fe0793b187852ffd98a33cfa2012db9
Sander van Rijn <svr003@gmail.com>
2021-12-14 01:03:55 +0100

    Added 2021 day 12

---
+++ Sander/2021/day12.py
@@ -0,0 +1,84 @@
+from collections import Counter, defaultdict
+from copy import copy
+from itertools import product
+from pprint import pprint
+
+from more_itertools import chunked
+import numpy as np
+import parse
+
+
+def a(edges):
+    options = set(edges.keys()) - {'start'}
+    route = ['start']
+    routes = build_single_visit_routes(route, options, edges)
+    return len(routes)
+
+
+def b(edges):
+    options = set(edges.keys()) - {'start'}
+    route = ['start']
+    routes = build_double_visit_routes(route, options, edges)
+    return len(routes)
+
+
+def build_single_visit_routes(route, options, edges):
+    if len(options) == 0:
+        return []
+    routes = []
+    for option in options:
+        if option in edges[route[-1]]:
+            new_route = copy(route) + [option]
+            if option == 'end':
+                routes.append(new_route)
+            elif option.islower():
+                routes.extend(build_single_visit_routes(new_route, options - {option}, edges))
+            else:
+                routes.extend(build_single_visit_routes(new_route, options, edges))
+    return routes
+
+
+def build_double_visit_routes(route, options, edges):
+    if len(options) == 0:
+        return []
+    routes = []
+    for option in options:
+        if option in edges[route[-1]]:
+            new_route = copy(route) + [option]
+            check_double_visit = is_double_visit_safe(new_route)
+            if not is_double_visit_safe(new_route):
+                continue
+            if option == 'end':
+                routes.append(new_route)
+            else:
+                routes.extend(build_double_visit_routes(new_route, options, edges))
+    return routes
+
+
+def is_double_visit_safe(route):
+    counts = [route.count(x) for x in set(route) if x.islower()]
+    at_most_one_visited_more_than_once = sum(count >= 2 for count in counts) < 2
+    none_visited_thrice = sum(count > 2 for count in counts) < 1
+    return at_most_one_visited_more_than_once and none_visited_thrice
+
+
+if __name__ == '__main__':
+    files = [
+        'input12-test1.txt',
+        'input12-test2.txt',
+        'input12-test3.txt',
+        'input12.txt',
+    ]
+    for filename in files:
+        print(filename)
+        with open(filename) as f:
+            lines = f.read().splitlines()  # multi-line file
+
+        edges = defaultdict(list)
+        for line in lines:
+            start, end = line.split('-')
+            edges[start].append(end)
+            edges[end].append(start)
+
+        print(f'A: {a(edges)}')
+        print(f'B: {b(edges)}')

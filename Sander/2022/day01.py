d412a62be771c8d635be79217e2b7631b83e7657
Sander van Rijn <svr003@gmail.com>
2022-12-01 20:02:29 +0100

    Added 2022 day 01

---
+++ Sander/2022/day01.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+
+def a(elves):
+    """Solve day {NUM} part 1"""
+    return max(sum(map(int, elf)) for elf in elves)
+
+
+def b(elves):
+    """Solve day {NUM} part 2"""
+    return sum(sorted(sum(map(int, elf)) for elf in elves)[-3:])
+
+
+def parse_file(f: Path):
+    """Parse the input file into relevant data structure"""
+    lines = f.read_text().split('\n\n')
+    return [elf.splitlines() for elf in lines]
+
+
+def main():
+    """Main function to wrap variables"""
+    files = [
+        'input01-test1.txt',
+        'input01.txt',
+    ]
+    for filename in files:
+        print(filename)
+        data = parse_file(Path(filename))
+
+        print(f'A: {a(data)}')
+        print(f'B: {b(data)}')
+
+
+if __name__ == '__main__':
+    main()

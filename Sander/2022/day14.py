3c21580f606ca0d4460d01dae696299e3da95ba9
Sander van Rijn <svr003@gmail.com>
2023-01-01 18:21:17 +0100

    refactor day 14 into functions

--- Sander/2022/day14.py
+++ Sander/2022/day14.py
@@ -8,19 +8,19 @@ ROCK = 1
 SAND = 2
 
 
-
-def a(shapes):
-    """Solve day 14 part 1"""
-    area = np.zeros((200,600))
+def place_shapes(shapes, size):
+    area = np.zeros(size)
     for shape in shapes:
         for (x1, y1), (x2, y2) in windowed(shape, n=2):
             if x2 < x1:
                 x1, x2 = x2, x1
             if y2 < y1:
                 y1, y2 = y2, y1
-            area[x1:x2+1, y1:y2+1] = ROCK
-    max_x = np.max(np.where(np.sum(area == ROCK, axis=1))[0])
+            area[x1:x2 + 1, y1:y2 + 1] = ROCK
+    return area
 
+
+def drop_sand(area, max_x):
     x, y = 0, 500
     while x <= max_x:
         if area[x+1, y] == AIR:
@@ -31,40 +31,25 @@ def a(shapes):
             x, y = x+1, y+1
         else:
             area[x, y] = SAND
+            if x == 0 and y == 500:
+                break
             x, y = 0, 500
 
+
+def a(shapes):
+    """Solve day 14 part 1"""
+    area = place_shapes(shapes, size=(200,600))
+    max_x = np.max(np.where(np.sum(area == ROCK, axis=1))[0])
+    drop_sand(area, max_x)
     return np.sum(area == SAND)
 
 
 def b(shapes):
     """Solve day 14 part 2"""
-    area = np.zeros((200,1_000))
-    for shape in shapes:
-        for (x1, y1), (x2, y2) in windowed(shape, n=2):
-            if x2 < x1:
-                x1, x2 = x2, x1
-            if y2 < y1:
-                y1, y2 = y2, y1
-            area[x1:x2+1, y1:y2+1] = ROCK
+    area = place_shapes(shapes, size=(200,1_000))
     max_x = np.max(np.where(np.sum(area == ROCK, axis=1))[0])
-
     area[max_x+2] = ROCK
-
-    x, y = 0, 500
-    while True:
-        if area[x+1, y] == AIR:
-            x += 1
-        elif area[x+1, y-1] == AIR:
-            x, y = x+1, y-1
-        elif area[x+1, y+1] == AIR:
-            x, y = x+1, y+1
-        elif x == 0 and y == 500:
-            area[x,y] = SAND
-            break
-        else:
-            area[x, y] = SAND
-            x, y = 0, 500
-
+    drop_sand(area, max_x+1)
     return np.sum(area == SAND)
 
 

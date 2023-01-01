fa0d054d8f86e09cff04c7b2d8cb4b7e323f64f7
Sander van Rijn <svr003@gmail.com>
2023-01-01 20:06:37 +0100

    refactor day 15 into functions

--- Sander/2022/day15.py
+++ Sander/2022/day15.py
@@ -5,8 +5,7 @@ import parse
 from tqdm import trange
 
 
-def a(sensors, target_y):
-    """Solve day 15 part 1"""
+def sensor_coverage_at_distance(sensors, target_y):
     ex_ranges = []
     for (sensx, sensy), (beacx, beacy) in sensors:
         dist = abs(sensx - beacx) + abs(sensy - beacy)
@@ -14,44 +13,36 @@ def a(sensors, target_y):
         if dist_at_ty > 0:
             ex_ranges.append((sensx-dist_at_ty, sensx+dist_at_ty))
     ex_ranges.sort(key=itemgetter(0))
+    return ex_ranges
 
+
+def merge_ranges(ex_ranges):
     idx = 0
     while idx < len(ex_ranges)-1:
         r1start, r1end = ex_ranges[idx]
         r2start, r2end = ex_ranges[idx+1]
         if r2end <= r1end:
             del ex_ranges[idx+1]
-        elif r2start <= r1end < r2end:
+        elif r2start <= r1end < r2end or r2start == (r1end+1):
             ex_ranges[idx] = (r1start, r2end)
             del ex_ranges[idx+1]
         else:
             idx += 1
+    return ex_ranges
 
+
+def a(sensors, target_y):
+    """Solve day 15 part 1"""
+    ex_ranges = sensor_coverage_at_distance(sensors, target_y)
+    ex_ranges = merge_ranges(ex_ranges)
     return sum(end-start for start, end in ex_ranges)
 
 
 def b(sensors, half_size):
     """Solve day 15 part 2"""
     for target_y in trange(2*half_size + 1):
-        ex_ranges = []
-        for (sensx, sensy), (beacx, beacy) in sensors:
-            dist = abs(sensx - beacx) + abs(sensy - beacy)
-            dist_at_ty = dist - abs(target_y - sensy)
-            if dist_at_ty > 0:
-                ex_ranges.append((sensx-dist_at_ty, sensx+dist_at_ty))
-        ex_ranges.sort(key=itemgetter(0))
-
-        idx = 0
-        while idx < len(ex_ranges)-1:
-            r1start, r1end = ex_ranges[idx]
-            r2start, r2end = ex_ranges[idx+1]
-            if r2end <= r1end:
-                del ex_ranges[idx+1]
-            elif r2start <= r1end < r2end or r2start == (r1end+1):
-                ex_ranges[idx] = (r1start, r2end)
-                del ex_ranges[idx+1]
-            else:
-                idx += 1
+        ex_ranges = sensor_coverage_at_distance(sensors, target_y)
+        ex_ranges = merge_ranges(ex_ranges)
 
         if len(ex_ranges) != 1:
             (_, r1end), (r2start, _) = ex_ranges
